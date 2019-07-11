a400cf1e35d7eefe42b7a4cf6b435dd0a29420d6
zhuhuan <huanq570@gmail.com>
2019-07-11 11:30:45 +0800

    修改内容

--- idea-git-test/src/HelloGit.java
+++ idea-git-test/src/HelloGit.java
@@ -2,5 +2,12 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
+
+
+    }
+    //test
+    public void test(){
+        int i = 1;
+        System.out.println(i);
     }
 }
